826d79ea6a5543d51ad07d8d6af7209469b19482
Kirk Pettinga <kirk.pettinga@gmail.com>
2024-03-04 12:37:44 -0500

    preliminary JS

--- public/index.js
+++ public/index.js
@@ -1,5 +1,55 @@
-(function(){
+(function () {
+	// FA Settings ----------------------------- //
+	window.wFORMS.behaviors.prefill.skip = false;
+	window.wFORMS.behaviors.paging.runValidationOnPageNext = true;
+	window.wFORMS.behaviors.paging.MESSAGES.CAPTION_NEXT = "Next";
+	window.wFORMS.behaviors.paging.MESSAGES.CAPTION_PREVIOUS = "Previous";
+	window.wFORMS.behaviors.paging.onPageChange = onPageChange;
 
-	console.log('up and running!');
 
+	// Local ----------------------------- //
+	let scrollY = window.scrollY;
+
+
+	// Handlers ----------------------------- //
+	document.addEventListener("DOMContentLoaded", function() {
+		customizeCaptcha();
+	});
+	window.addEventListener("scroll", throttle(function() {
+		// counteracts FA's behavior which scrolls window 
+		// to the top of the current form page.
+		scrollY = window.scrollY
+	}, 100))
+
+
+	// Methods ----------------------------- //
+
+	function onPageChange($page) {
+		window.scrollTo(0, scrollY);
+	}
+
+	function customizeCaptcha() {
+		// Target captchaExplanation
+		let captchaExplanation = document.querySelector("#disabled-explanation");
+		// Check if the element exists
+		if (captchaExplanation) {
+			// Change the text of the element
+			captchaExplanation.innerHTML =
+			'The submit button will be disabled until you complete the CAPTCHA.<span style="color: red;">*</span>';
+		}
+	}
+
+	function throttle(func, limit) {
+		let inThrottle;
+		return function() {
+			const context = this;
+			const args = arguments;
+			if (!inThrottle) {
+				func.apply(context, args);
+				inThrottle = true;
+				setTimeout(() => inThrottle = false, limit);
+			}
+		};
+	}
+	
 })();
